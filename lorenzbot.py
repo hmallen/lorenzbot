9903cd3ac407d22cf0695385962bc14bb3cda9e5
hmallen <allenhm@gmail.com>
2017-11-29 02:25:14 -0600

    Fixing output.

--- lorenzbot.py
+++ lorenzbot.py
@@ -614,20 +614,13 @@ def telegram_status(bot, update):
 
     if connected_users.count(telegram_user) > 0:
         logger.debug('Access confirmed for requesting user.')
-        
-        base = str(calc_base())
-        remain_usdt = "{:.2f}".format(trade_usdt_remaining)
-        spent = calc_trade_totals('spent')
-        bought = calc_trade_totals('bought')
-        rate = spent / bought
-        
-        spent_msg = 'USDT Spent: ' + "{:.4f}".format(spent) + '\n'
-        bought_msg = 'STR Bought: ' + "{:.4f}".format(bought) + '\n'
-        rate_msg = 'Avg. Rate: ' + "{:.4f}".format(rate) + '\n'
-        base_msg = 'Base Price: ' + "{:.4f}".format(base_price) + '\n'
+                
+        spent_msg = 'USDT Spent:  ' + "{:.4f}".format(calc_trade_totals('spent')) + '\n'
+        bought_msg = 'STR Bought: ' + "{:.4f}".format(calc_trade_totals('bought')) + '\n'
+        rate_msg = 'Base Price:   ' + "{:.4f}".format(base_price) + '\n'
         market_msg = 'Mkt. Price: ' + "{:.4f}".format(polo.returnTicker()['USDT_STR']['last'])# + '\n'
 
-        telegram_message = spent_msg + bought_msg + rate_msg + base_msg + market_msg
+        telegram_message = spent_msg + bought_msg + rate_msg + market_msg
 
     else:
         logger.warning('Access denied for requesting user.')
