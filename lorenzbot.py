7ec7415856362bddaa0c3820b72b4c84f69ece52
hmallen <allenhm@gmail.com>
2017-12-03 01:46:57 -0500

    Fixed write/append on exception logging.

--- lorenzbot.py
+++ lorenzbot.py
@@ -1378,12 +1378,12 @@ if __name__ == '__main__':
             logger.exception('Exception raised and caught in main loop.')
             logger.exception(e)
             
-            with open(exception_log_file, 'w') as ex_file:
-                ex_file.write(datetime.datetime.now().strftime('%m%d%Y-%H%M%S') + ': ' + str(e))
+            with open(exception_log_file, 'a') as ex_file:
+                ex_file.write(datetime.datetime.now().strftime('%m%d%Y-%H%M%S') + ': ' + str(e) + '\n')
                 logger.debug('Wrote exception to exception log file.')
 
-            with open(exception_last_file, 'a') as ex_file:
-                ex_file.write(datetime.datetime.now().strftime('%m%d%Y-%H%M%S') + ': ' + str(e))
+            with open(exception_last_file, 'w') as ex_file:
+                ex_file.write(datetime.datetime.now().strftime('%m%d%Y-%H%M%S') + ': ' + str(e) + '\n')
                 logger.debug('Wrote exception to recent exception file.')
 
             if telegram_active == True:
